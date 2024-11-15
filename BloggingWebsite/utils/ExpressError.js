236432f83cabdc182512a959d67f69091dc6ef12
Maneeya Mehta <133096607+maneeya04@users.noreply.github.com>
2024-11-15 09:34:23 +0530

    Update ExpressError.js

--- BloggingWebsite/utils/ExpressError.js
+++ BloggingWebsite/utils/ExpressError.js
@@ -1,5 +1,11 @@
-module.exports = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
+class ExpressError extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = message;
+        this.statusCode = statusCode;
     }
+
 }
+
+module.exports = ExpressError;
+
